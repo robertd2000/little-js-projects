7fe0cf7a9b3ddb99fc7b79e0d8bacd06f8ba42a8
robert d <zdabaghov@mail.ru>
2021-12-20 20:31:49 +0300

    quotes add some styles

--- quotes/script.js
+++ quotes/script.js
@@ -4,25 +4,26 @@ let quoteBodyInner = quoteBody.querySelector('.quote_body_inner')
 let authorDom = document.querySelectorAll('.author')
 let tagsDom = document.querySelector('.tags')
 let tagLabel = document.querySelector('.tag_label')
+let loader = document.querySelector('.loader')
 
 
 const getQuote = async () => {
+    loader.style.display = 'block'
     const response = await (await fetch(URL)).json()
+    loader.style.display = 'none'
+
     return quoteHandler(response)
 }
 
 const quoteHandler = (data) => {
-    console.log(data)
     quoteBody.textContent = `“${data.quote.body}”` 
-    quoteBody.cite = data.quote.url
-       
-        authorDom.forEach(item =>  item.textContent += data.quote.author)
+    quoteBody.cite = data.quote.url 
+    authorDom.forEach(item =>  item.textContent += data.quote.author)
     
     const tags = data.quote.tags
     let res = tags.length > 0 ? 'tags: ' : ''
 
     tags && tags.forEach((tag) => {
-        console.log(tag);
         let tagBody = `<a class="tag" href="https://favqs.com/quotes/tag/${tag}">&#9750;${tag}</a> `
         res += tagBody + '   '
         tagsDom.innerHTML = res
