f67543d5610cd05991fbb1afdafbf2a95999b2d0
robert d <zdabaghov@mail.ru>
2021-12-21 14:43:34 +0300

    simple clock add styles and themes

--- clock/script.js
+++ clock/script.js
@@ -1,3 +1,29 @@
+let switcher = document.querySelector('.switch')
+
+switcher.addEventListener('change', (e) => {
+    console.log(e.target.checked);
+    if (e.target.checked) {
+        console.log('tre');
+        document.body.style.background = `conic-gradient(
+            from 90deg at 25% -10%,
+                #ff4500,
+                #d3f340,
+                #7bee85,
+                #afeeee,
+                #7bee85 
+          )`
+    } else {
+        document.body.style.background = `conic-gradient(
+            from 90deg at 25% -10%,
+                #ff00ea,
+                #ae13eb,
+                #4215e7,
+                #04cece,
+                #269e30
+          )`
+    }
+})
+
 const generateClocks = () => {
     let clockInner = document.querySelector('.clock_inner')
     let day = document.createElement('h2')
